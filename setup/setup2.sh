a1c7897fc6eb1a797dd4c6550e20649c6bcd7788
ashwarn <ashwarn@gmail.com>
2020-03-03 09:25:51 +1100

    Update setup2.sh

--- setup/setup2.sh
+++ setup/setup2.sh
@@ -16,29 +16,37 @@ ansible-playbook setup.yml
 
 cp ~/.ansible/multi-planet-configurations/templates/apigee-opdk-configuration-template.cfg ~/.ansible/multi-planet-configurations/prod.cfg
 
-cp -r ~/.ansible/inventory/templates/edge-aio/ ~/.ansible/inventory/prod/
+cp -r ~/.ansible/inventory/templates/edge-2/ ~/.ansible/inventory/prod/
 
 export ANSIBLE_CONFIG=~/.ansible/multi-planet-configurations/prod.cfg
 
-sed -i 's/UPDATE_WITH_SSH_USER_NAME/osboxes/g'  ~/.ansible/multi-planet-configurations/prod.cfg
+USER=`whoami`
+
+
+
+sed -i "s/UPDATE_WITH_SSH_USER_NAME/$USER/g"  ~/.ansible/multi-planet-configurations/prod.cfg
 sed -i 's/TARGET_ENVIRONMENT_NAME_CONVENTION/prod/g'  ~/.ansible/multi-planet-configurations/prod.cfg
 
-sed -i 's/10.x.x.x/localhost/g' ~/.ansible/inventory/prod/inventory-aio
+sed -i "s/10.x.x.x/$HOSTNAME/g" ~/.ansible/inventory/prod/edge-dc1
 
 cd ~/ansible-opdk-accelerator/installations/multi-node
 
-ssh-keygen -t rsa 
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -b 2048 -t rsa -f /tmp/id_rsa_ansible -q -N "" 0>&-
+
+cat ~/.ssh/id_rsa_ansible.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
+sed -i 's/id_rsa/id_rsa_ansible/g'  ~/.ansible/multi-planet-configurations/prod.cfg
+
+cd ansible-opdk-accelerator/installations/multi-node
 ansible-galaxy install -r requirements.yml -f
 ansible-playbook install-optimized.yml
 
 
-vi ~/.apigee/custom-properties.yml
+# vi ~/.apigee/custom-properties.yml
 
-change the version 
+# change the version 
 
-mkdir ~/apigee-4.19.06
+# mkdir ~/apigee-4.19.06
 
-edit /home/osboxes/.apigee/apigee-mirror-archive-properties.yml
+# edit /home/osboxes/.apigee/apigee-mirror-archive-properties.yml
