444d0377a060d101ab9bf7cd3e3fb8111618a3d3
Abhishek Singh <absingh@transurban.com>
2021-04-15 16:31:05 +1000

    removed bootstap

--- setup/setup-rmp-azure-optimized-demo.sh
+++ setup/setup-rmp-azure-optimized-demo.sh
@@ -1,7 +1,10 @@
 export ANSIBLE_CONFIG=~/.ansible/multi-planet-configurations/prod.cfg
 cd ~/ansible-opdk-accelerator/installations/multi-node
 
-ansible-playbook install-optimized.yml --tags cache,response-file
-ansible-playbook install-optimized.yml --tags copy --limit apigee_004
-ansible-playbook install-optimized.yml --tags bootstrap --limit apigee_004
+sed -i "/^apigee_004/s/10.x.x.x/`hostname -i`/g" ~/.ansible/inventory/prod/edge-dc1
+
+
+# ansible-playbook install-optimized.yml --tags cache,response-file
+# ansible-playbook install-optimized.yml --tags copy --limit apigee_004
+# ansible-playbook install-optimized.yml --tags bootstrap --limit apigee_004
 ansible-playbook install-optimized.yml --tags rmp --limit apigee_004
