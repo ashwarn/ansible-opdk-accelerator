399b5ffec541d8bced40e6934f1de7cb69a63c8e
Abhishek Singh <absingh@transurban.com>
2021-04-16 18:21:33 +1000

    removed extra var

--- setup/setup-rmp-azure-optimized-demo.sh
+++ setup/setup-rmp-azure-optimized-demo.sh
@@ -4,7 +4,7 @@ cd ~/ansible-opdk-accelerator/installations/multi-node
 sed -i "/^apigee_004/s/10.x.x.x/`hostname -i`/g" ~/.ansible/inventory/prod/edge-dc1
 
 
-# ansible-playbook install-optimized.yml --tags cache,response-file
+ansible-playbook install-optimized.yml --tags cache,response-file
 # ansible-playbook install-optimized.yml --tags copy --limit apigee_004
 # ansible-playbook install-optimized.yml --tags bootstrap --limit apigee_004
-ansible-playbook install-optimized.yml --tags rmp --limit apigee_004 -e "target_response_file_path: {{ opdk_resources_path }}/{{ response_file_name }}"
+ansible-playbook install-optimized.yml --tags rmp --limit apigee_004
