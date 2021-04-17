e04e3059d4a6d3f70f157bd64dab67e4a761542f
ashwarn <abhishek.ash.singh@accenture.com>
2021-04-17 15:00:13 +1000

    allow firewall

--- setup/test-nginx.sh
+++ setup/test-nginx.sh
@@ -1,3 +1,5 @@
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y nginx
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
 systemctl start nginx
