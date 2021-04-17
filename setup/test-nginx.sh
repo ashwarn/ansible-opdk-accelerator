f227677ce3568cda6e036b98569ea6db0850ad9d
ashwarn <abhishek.ash.singh@accenture.com>
2021-04-17 10:57:16 +1000

    yum

--- setup/test-nginx.sh
+++ setup/test-nginx.sh
@@ -1,3 +1,3 @@
-yum install epel-release
-apt-get install -y nginx
+yum install epel-release -y
+yum install -y nginx
 echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
