3af3f088dcd76212c0fec8759dc494006cd23376
ashwarn <abhishek.ash.singh@accenture.com>
2021-04-17 11:26:15 +1000

    test

--- setup/test-nginx.sh
+++ setup/test-nginx.sh
@@ -1,3 +1,4 @@
-yum install epel-release -y
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install epel-release-latest-7.noarch.rpm -y
 yum install -y nginx
 echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
